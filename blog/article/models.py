069741152b4c96fe90f3e9bfa14616f8cc83173d
allen <allen@m516-HP-EliteDesk-800-G1-TWR>
2016-11-11 10:09:21 +0800

    end ch7

--- blog/article/models.py
+++ blog/article/models.py
@@ -14,6 +14,7 @@ class Article(models.Model):
 class Comment(models.Model):
     article = models.ForeignKey(Article)
     content = models.CharField(max_length=128)
+    pubDateTime = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.article.title + '-' + str(self.id)
